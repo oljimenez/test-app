81708639c7cd6a7be62520be3b783ed144c7d03c
oljimenez <oljimenez.prog@gmail.com>
2025-12-09 03:20:48 -0500

    WIP

---
+++ app/[id]/layout.tsx
@@ -0,0 +1,18 @@
+
+export default  function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+ 
+
+//   throw new Error("Test error from layout");
+
+
+
+  return (
+    <div >
+        {children}
+    </div>
+  );
+}
--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,12 +1,21 @@
+import { Suspense } from "react";
 
-"use cache"
+export default async function Home(props: PageProps<"/[id]">) {
+  
+    return (
+        <Suspense fallback={<div>Loading...</div>}> 
+            <CachedData params={props.params}/>
+        </Suspense>
+     )
+}
 
-export default async function Home(props: {params: Promise<{ id: string }>}) {
+async function CachedData(props:{params: PageProps<"/[id]">['params']}){
+    "use cache"
+    const params = await props.params;
     const res =  await fetch("https://dog.ceo/api/breeds/image/random").then((res) => res.json());
 
-    return (
-        <div> 
-            <img src={res.message} alt="A Random Dog" width={400} height={400} />
-        </div>
-     )
+    return  <div> 
+        <img src={res.message} alt="A Random Dog" width={400} height={400} />
+        {params.id}
+    </div>
 }
