c7e9baf6374e38955aa9ab3447a21f24f6ad925f
oljimenez <oljimenez.prog@gmail.com>
2025-12-09 03:51:09 -0500

    WIP

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,24 +1,31 @@
 import { cacheLife } from "next/cache";
+import { Suspense } from "react";
 
 
-export default async function Home(props: PageProps<"/[id]">) {
+export default function Home(props: PageProps<"/[id]">) {
   
     return (
         <div> 
-            <CachedData params={props.params}/>
+            <Suspense fallback={<div>Loading...</div>}>
+                <RuntimeData params={props.params}/>
+            </Suspense>
+            <CachedData />
         </div>
      )
 }
 
-async function CachedData(props:{params: PageProps<"/[id]">['params']}){
+async function RuntimeData(props: {params: PageProps<"/[id]">['params']}){
+    const params = await props.params;
+    return <div>{params.id}</div>
+}
+
+async function CachedData(){
     "use cache";
     cacheLife('max');
 
-    const params = await props.params;
     const res =  await fetch("https://dog.ceo/api/breeds/image/random").then((res) => res.json());
 
     return  <div> 
         <img src={res.message} alt="A Random Dog" width={400} height={400} />
-        {params.id}
     </div>
 }
