05ea201589d08935e430f53e8eb6801637f1f0d5
oljimenez <oljimenez.prog@gmail.com>
2025-12-09 01:29:43 -0500

    WIP

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    logging:{
+        fetches: {
+            fullUrl:true, 
+            hmrRefreshes:true
+        },
+        incomingRequests:true
+    },
+
   /* config options here */
 };
 
