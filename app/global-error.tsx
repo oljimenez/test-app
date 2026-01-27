88d42219562999b7eadd80f0b70419d94188ef50
oljimenez <oljimenez.prog@gmail.com>
2026-01-27 15:22:15 -0500

    feat(error): update global error file

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,18 +1,18 @@
 'use client' // Error boundaries must be Client Components
  
 export default function GlobalError({
-  error,
-  reset,
+  error:__,
+  reset:_,
 }: {
   error: Error & { digest?: string }
   reset: () => void
 }) {
   return (
-    // global-error must include html and body tags
+   
     <html>
       <body>
         <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <p>This is a global error file</p>
       </body>
     </html>
   )
